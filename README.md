8a9bafdb9a75a44556f23965c42b086938a2aaef
RogueLeader <sgshami@gmail.com>
2022-12-15 01:34:34 -0800

    Install vue and Create page template
    Vue was installed and created. The base template was set up. Includes: routing options, vue options, and landing/homepage template.

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
 # E-commerce-Website-With-Django-and-Vue-
 
-To Create the Vitual Environment:
-py -m venv %name%
+To Create the Vitual Environment: py -m venv %name%
 
-Activate the Environment on Windows by going to the directory and input:
-project\Scripts\activate.bat
+Activate the Environment on Windows by going to the directory and input: project\Scripts\activate.bat
 
-Deactivate by input:
-deactivate
+Deactivate by input: deactivate
+
+To Run the backend server: python manage.py runserver
+
+To Run Vue: npm run serve
---
+++ project/djackets_vue/src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="wrapper">
+    <!-- Navigation Bar: -->
+    <nav class="navbar is-dark">
+      <!-- Logo: -->
+      <div class="navbar-brand">
+        <router-link to="/" class="navbar-item"><strong>SS Tech</strong></router-link>
+
+        <a class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbar-menu">
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
+      </div>
+
+      <div class="navbar-menu" id="navbar-menu">
+        <div class="navbar-end">
+          <router-link to="/computers" class="navbar-item">Computer</router-link>
+          <router-link to="/tablets" class="navbar-item">Tablets</router-link>
+          <router-link to="/phones" class="navbar-item">Phones</router-link>
+          <router-link to="/wearables" class="navbar-item">Wearables</router-link>
+          
+          <div class="navbar-item">
+            <div class="buttons">
+              <router-link to="log-in" class="button is-light">Log In</router-link>
+              <rotuer-link to="/cart" class="button is-success">
+                <span class='icon'><i class="fas fa-shopping-cart"></i>
+                </span><span>Cart</span>
+              </rotuer-link>
+            </div>
+          </div>
+
+        </div>
+      </div>
+
+    </nav>
+    <section class="section">
+      <router-view/>
+    </section>
+    
+    <footer class="footer">
+      <p class="has-text-centered">Copyright (c) 2022</p>
+    </footer>
+  </div>
+
+</template>
+
+<style lang="scss">
+@import '../node_modules/bulma'
+</style>
